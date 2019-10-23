668785b340a30a6971c2d5145114320bb4719867
valterbarros <valterinside@gmail.com>
2019-10-23 00:03:40 -0300

    master-add-changes
    Signed-off-by: valterbarros <valterinside@gmail.com>

---
+++ list_and_recursion_7.ex
@@ -0,0 +1,3 @@
+def ListAndRecursion do
+
+end
--- parse_string.ex
+++ parse_string.ex
@@ -1,4 +1,16 @@
 # digit - ?0 is to know the value of number because when make [digit|tail] = '1,2,3' digit return integer that represent 1
+#
+# Example:
+#
+# iex(8)> 234
+# 234
+# iex(10)> 0 * 10 + ?2 - ?0
+# 2
+# iex(11)> 2 * 10 + ?3 - ?0
+# 23
+# iex(12)> 23 * 10 + ?4 - ?0
+# 234
+# iex(13)> 
 
 defmodule Parse do
   def number([ ?- | tail ]), do: _number_digits(tail, 0) * -1
@@ -8,7 +20,7 @@ defmodule Parse do
   defp _number_digits([ digit | tail ], value)
   when digit in '0123456789' do
     IO.puts(digit)
-    _number_digits(tail, value*10 + digit - ?0)
+    _number_digits(tail, value * 10 + digit - ?0)
   end
   defp _number_digits([ non_digit | _ ], _) do
     raise "Invalid digit '#{[non_digit]}'"
